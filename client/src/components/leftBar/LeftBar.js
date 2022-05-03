84893d33f5d9ec9162fc86a3651f9bd7cbc840ed
lokendra <lokendrachaulagain803@gmail.com>
2022-05-03 08:44:50 +0545

    leftnav

---
+++ client/src/components/leftBar/LeftBar.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./leftBar.scss"
+import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
+import HelpCenterOutlinedIcon from '@mui/icons-material/HelpCenterOutlined';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import HomeOutlined from '@mui/icons-material/HomeOutlined';
+
+
+
+
+
+function LeftBar() {
+  return (
+    <div className='leftBar'>
+      <div className="lbWrapper">
+
+        <div className="lbListItem">
+          <HomeOutlined className="lbListItemKey"/>
+          <span className="lbListItemValue">Home</span>
+        </div>
+
+        <div className="lbListItem">
+          <BookOutlinedIcon className="lbListItemKey"/>
+          <span className="lbListItemValue">Blogs</span>
+        </div>
+
+        <div className="lbListItem">
+          <WhatshotIcon className="lbListItemKey"/>
+          <span className="lbListItemValue">Trending</span>
+        </div>
+
+        <div className="lbListItem">
+          <QuestionMarkIcon className="lbListItemKey"/>
+          <span className="lbListItemValue">Ask Doubts</span>
+        </div>
+
+        <div className="lbListItem">
+          <AutoStoriesIcon className="lbListItemKey"/>
+          <span className="lbListItemValue">Success Story</span>
+        </div>
+
+        <div className="lbListItem">
+          <HelpCenterOutlinedIcon className="lbListItemKey"/>
+          <span className="lbListItemValue">Help </span>
+        </div>
+
+        <div className="lbListItem">
+          <LogoutIcon className="lbListItemKey"/>
+          <span className="lbListItemValue">Logout </span>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default LeftBar
