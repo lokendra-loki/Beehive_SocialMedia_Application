e0fa86ba56965eadaf4fe1c21abb5a80ea5d51d2
lokendra <lokendrachaulagain803@gmail.com>
2022-05-05 07:28:04 +0545

    userLogin

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 
 
 
+
 //REGISTER
 router.post("/register", async (req, res) => {
     try {
@@ -13,7 +14,7 @@ router.post("/register", async (req, res) => {
 
         //generate salt
         const salt = await bcrypt.genSalt(10)
-        
+
         //hash the password
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
@@ -30,6 +31,43 @@ router.post("/register", async (req, res) => {
         //save user
         const savedUser = await newUser.save()
         res.status(201).json(savedUser)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        //find user by email
+        const user = await User.findOne({ email: req.body.email })
+
+        //if user exists check password
+        if (user) {
+            const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+            //if password match create token
+            if (validPassword) {
+                //create token
+                const accessToken = jwt.sign({
+                    id: user._id,
+                    isAdmin: user.isAdmin
+
+                }, process.env.JWT_SECRET, { expiresIn: '3d' })
+
+                const { password, ...others } = user._doc;
+                res.status(200).json({others, accessToken})
+            }
+            else {
+                res.status(401).json("Invalid Password")
+            }
+        }
+        else {
+            res.status(401).json("User not found")
+        }
 
     } catch (error) {
         res.status(500).json(error)
