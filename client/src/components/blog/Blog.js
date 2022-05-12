b6fa72f6fb0b3a791f8956ec6a1ae97b45b558f9
lokendra <lokendrachaulagain803@gmail.com>
2022-05-12 11:16:33 +0545

    fetchedPost all and set

--- client/src/components/blog/Blog.js
+++ client/src/components/blog/Blog.js
@@ -1,8 +1,8 @@
-import React from 'react'
 import "./blog.scss"
 
 
-function Blog() {
+
+function Blog({ blog }) {
     return (
         <div className='blog'>
             <img src="/assets/cover.jpeg" alt="" className="blogImg" />
@@ -10,14 +10,14 @@ function Blog() {
             <div className="blogInsideCon">
                 <div className="blogDateAndTimeRead">
                     <span className="blogDate">24 jan, 2022</span>
-                    <span className="blogTimeRead">3 min read</span>
+                    <span className="blogTimeRead">{blog.timeRead}</span>
                 </div>
-                <span className="blogTitle">Usinga Social Medias Properly for Business and sports that hepls yo acxcount for th globe</span>
+                <span className="blogTitle">{blog.title} </span>
                 <div className="blogAuthorAndLocation">
-                    <span className="blogAuthor"><span className='author'>Author</span> : Loki Chaulagain</span>
-                    <span className="blogLocation">Kathmandu</span>
+                    <span className="blogAuthor"><span className='author'>Author</span> : {blog.author}</span>
+                    <span className="blogLocation">{blog.location} </span>
                 </div>
-                <span className="blogDesc">Lorem ipsum uifng  quisquam sequi corporis uifng  quisquam sequi corporis  voluptates praesentium porro magni delectus quae soluta doloremque accusantium, dignissimos odit, quas tempora explicabo iusto ducimus nihil?</span>
+                <span className="blogDesc">{blog.desc} </span>
                 <hr className='blogHr' />
                 <span className="blogContinueReading">Continue reading...</span>
             </div>
--- client/src/pages/blogs/Blogs.js
+++ client/src/pages/blogs/Blogs.js
@@ -1,35 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Blog from '../../components/blog/Blog'
 import Navbar from '../../components/navbar/Navbar'
+import axios from 'axios'
 import "./blogs.scss"
 
 
+
+
 function Blogs() {
+
+  //Fetching all blogs
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const res = await axios.get("/blogs/getAll")
+      console.log(res.data)
+      setBlogs(res.data)
+    }
+    fetchBlogs()
+  }, [])
+
+
+
   return (
     <div className='blogs'>
       <Navbar />
       <div className="header">
         This is header
       </div>
-
       <div className="blogsWrapper">
         <div className="blogsLeftSide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eos pariatur iure, blanditiis, esse, maxime totam laborum maiores autem tenetur nobis commodi necessitatibus corrupti? Expedita eligendi veniam ab similique aliquid officia totam dolores velit omnis minus beatae, a, in voluptatum libero consectetur fugit aperiam nulla laborum doloremque repellat aspernatur inventore, pariatur eum! Non perspiciatis debitis aliquid, sit quod temporibus alias odio sint veritatis. Labore, inventore maxime. Quas consectetur repudiandae esse eligendi veniam provident iusto molestiae quam, et praesentium accusamus at voluptatibus nisi. Veritatis aspernatur magnam consequuntur, molestiae officia aut nostrum nisi iste repellat excepturi reiciendis laborum. Molestias obcaecati ea quia quam consequatur quaerat voluptatem sapiente? Temporibus, iusto ipsum possimus facilis quis, deserunt totam dolor maxime fugit esse, exercitationem nam beatae in voluptatum cupiditate eum veritatis rerum aspernatur voluptatem! Ullam iure sit aperiam dignissimos natus nobis expedita maxime ad explicabo possimus libero veritatis praesentium sunt ratione, quia vitae eligendi, quibusdam aliquid neque nisi rem? Vel inventore fuga nihil consectetur debitis non distinctio aspernatur aliquid totam, suscipit repellendus, qui vitae placeat dolores ad molestias nemo praesentium atque veritatis temporibus numquam! Nihil, eum asperiores illum cum, quas iusto porro aliquid a nesciunt reprehenderit beatae dicta ab officia vel! Dicta accusamus fugiat aut debitis sed laborum esse nisi ipsam, voluptas animi delectus molestiae consequuntur! Cum, repellat. Culpa natus minima aliquid maxime adipisci voluptates praesentium tempore debitis excepturi, tenetur doloremque mollitia, dolores numquam perferendis repellendus cumque necessitatibus hic consequatur consectetur deleniti officia ea. Dolorum aut natus eaque adipisci delectus ipsam illo doloribus necessitatibus laudantium animi deserunt impedit illum in aspernatur et neque enim perferendis reprehenderit consectetur, a ducimus molestias. Doloremque, exercitationem quas porro modi aperiam minima dolor molestias possimus incidunt voluptatum reprehenderit tempora magnam delectus ipsum perferendis laudantium, quis eaque vel quidem quae, neque nisi odit deserunt rem. Totam voluptates impedit eveniet odio dolorum delectus alias expedita sed nobis aut, beatae commodi at voluptatem dolor aspernatur suscipit exercitationem accusamus quas blanditiis voluptatibus molestiae magnam? Vitae quam sit, aut dolorem blanditiis nobis maiores, nihil ipsum consequatur, rerum dolores esse nisi assumenda sint eaque rem numquam natus. Commodi aliquid, sed, molestias placeat eum totam quibusdam beatae veniam eligendi est sequi quos eveniet aperiam quam. Iste, consequuntur commodi. Maxime excepturi delectus deleniti harum in id sit blanditiis, molestiae aspernatur quis ipsam aut quia possimus soluta modi doloribus corrupti qui! Autem quaerat, dolorum similique officia consequatur neque dolor praesentium dolore illum velit minus dolorem dignissimos modi debitis placeat animi ratione eveniet inventore nemo voluptatibus! Mollitia reprehenderit animi quas vel? Ut totam nisi inventore corrupti quo. Sunt doloremque quam tempore accusamus, est consequatur harum natus cum laudantium placeat sit doloribus! Perspiciatis earum id optio quae nostrum eveniet debitis quaerat ipsam ducimus, explicabo quibusdam quod aliquam consequatur placeat enim asperiores reiciendis quo, reprehenderit autem incidunt. Tenetur cupiditate mollitia vitae dolores distinctio aliquam dolorum unde dicta tempore sit nihil officia illum doloremque impedit, expedita commodi eligendi, eaque, ducimus provident. Provident asperiores culpa facilis vel nihil nemo quod molestiae minima voluptatum, quia sunt officiis eveniet exercitationem obcaecati debitis rerum eos illo autem. Animi.</div>
 
-
-
-
-
         <div className="blogsRightSide">
-          <Blog/>
-          <Blog/>
-          <Blog/>
-          
+          {blogs.map((blog, key) => (
+            <Blog key={key} blog={blog} />
+          ))}
         </div>
 
       </div>
-
-
-
     </div>
   )
 }
