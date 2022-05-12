bc1a7bd7e4a2e8323721bbdec9b8917b6f9612a3
lokendra <lokendrachaulagain803@gmail.com>
2022-05-12 21:03:17 +0545

    done for today

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.scss"
 import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom'
+
+
+
 
 function Navbar() {
+
+
+
+
+
     return (
         <div className='navbar'>
+
             <img src="/assets/logo.svg" alt="" className="navbarLogo" />
 
             <div className="navbarLinks">
                 <span className='navLink'>Home</span>
                 <span className='navLink'>About </span>
-                <span className='navLink'>Contact</span>
-                <span className='navLink'>Forum</span>
-                <span className='navLink'>Jobs</span>
+                <span className='navLink'  >Contact</span>
+
+                <span className='navLink'>Blog</span>
+
+                {/* <div className="blogDropdown">
+                    <span className='dropDownNavLink'>Read Blogs</span>
+                    <span className='dropDownNavLink'>Write Blogs </span>
+                </div> */}
+
+
+
+
+
+
+                <Link to="/jobSearch" className='link'>
+                    <span className='navLink'>Jobs</span>
+                </Link>
+
+
+
             </div>
 
             <div className="navSearchCon">
@@ -21,8 +48,14 @@ function Navbar() {
             </div>
 
             <div className="navbarButtons">
-                <button className="navbarButton">Sign Up</button>
-                <button className="navbarButton  navbarButton1">Log In</button>
+
+                <Link to="/register">
+                    <button className="navbarButton">Sign Up</button>
+                </Link>
+
+                <Link to="/login">
+                    <button className="navbarButton  navbarButton1">Log In</button>
+                </Link>
             </div>
         </div>
 
--- client/src/pages/feedPage/FeedPage.js
+++ client/src/pages/feedPage/FeedPage.js
@@ -1,37 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./feedPage.scss"
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import OndemandVideoOutlinedIcon from '@mui/icons-material/OndemandVideoOutlined';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import Badge from '@mui/material/Badge';
+// import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+// import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+// import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+// import OndemandVideoOutlinedIcon from '@mui/icons-material/OndemandVideoOutlined';
+// import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+// import Badge from '@mui/material/Badge';
 import FeedPost from '../../components/feedPost/FeedPost';
 import Navbar from '../../components/navbar/Navbar';
 import LeftBar from '../../components/leftBar/LeftBar';
+import FeedPostCreate from '../../components/feedPostCreate/FeedPostCreate';
 
 
 
 function FeedPage() {
+
+
+    //To open the FeedPostCreateContainer
+    const [showFeedPostCreateCon, setShowFeedPostCreateCon] = useState(false)
+
+
+
     return (
         <div className="feedPage">
             <div className="fpWrapper">
-                <Navbar/>
-                {/* <div className="fpCoverPicCon">
-                    <img src="/assets/cover.jpeg" alt="" className="fpCoverPic" />
-                    <img src="/assets/profile.jpeg" alt="" className="fpProfilePic" />
-                </div> */}
-
+                <Navbar />
                 <div className="fpBeforeSplit">
 
                     <div className="fpLeftCon">
-                        {/* <UserInfo /> */}
-                        <LeftBar/>
+                        <LeftBar />
                     </div>
 
-                    
                     <div className="fpCenterCon">
-                        <div className="fpRowIconCon">
+
+                        <div className="smallWhatsOnYourMindCon">
+                            <img src="assets/profile.jpeg" alt="" className="tinyProfilePic" />
+                            <input onClick={()=>setShowFeedPostCreateCon(!showFeedPostCreateCon)} type="text" className="onYourMindInput"  placeholder="What's on your mind Lokendra ?  Post photos / videos / text from here"/>
+                        </div>
+                        {/* On Click ma yo container show hunxa */}
+                        {showFeedPostCreateCon && <FeedPostCreate/>}
+
+
+
+
+
+
+
+
+
+                        {/* <div className="fpRowIconCon">
                             <div className="fpRowIconItem">
                                 <Badge badgeContent={4} color="primary">
                                     <HomeOutlinedIcon color="action" />
@@ -61,30 +78,15 @@ function FeedPage() {
                                     <OndemandVideoOutlinedIcon color="action" />
                                 </Badge>
                             </div>
-                        </div>
-
+                        </div> */}
                         <FeedPost />
-                        
                     </div>
                     <div className="fpRightCon"></div>
                 </div>
-
-
-
-
-
-
-
             </div>
-
         </div>
     )
 
 }
-
-
-
-
-
 
 export default FeedPage
