4c64efe37d45d2a979a86ede91d7bab2010f51f2
lokendra <lokendrachaulagain803@gmail.com>
2022-05-14 11:28:27 +0545

    userdata fetched

--- client/src/components/userInfo/UserInfo.js
+++ client/src/components/userInfo/UserInfo.js
@@ -1,176 +1,198 @@
-import React, { useContext, useEffect, useState } from 'react'
-import "./userInfo.scss"
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import SchoolIcon from '@mui/icons-material/School';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import NetworkWifi2BarIcon from '@mui/icons-material/NetworkWifi2Bar';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import InsertLinkIcon from '@mui/icons-material/InsertLink';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import Hobby from '../hobby/Hobby';
-import { AuthContext } from '../../context/authContext/AuthContext';
-import UserInfoEdit from '../userInfoEdit/UserInfoEdit';
-import axios from 'axios';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import "./userInfo.scss";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import SchoolIcon from "@mui/icons-material/School";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import NetworkWifi2BarIcon from "@mui/icons-material/NetworkWifi2Bar";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Hobby from "../hobby/Hobby";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import UserInfoEdit from "../userInfoEdit/UserInfoEdit";
+import axios from "axios";
 
 function UserInfo() {
-    const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
-    //To open userInfoEditContainer
-    const [showUserInfoEditCon, setShowUserInfoEditCon] = useState(false)
+  //To open userInfoEditContainer
+  const [showUserInfoEditCon, setShowUserInfoEditCon] = useState(false);
 
-    //Fetching userDetail 
-    const [currentUser, setCurrentUser] = useState({});
-    useEffect(() => {
-        const userDetailsOny = async () => {
-            try {
-                const res = await axios.post("/userDetails/get", {
-                    userID: user._id
-                })
-                setCurrentUser(res.data[0])
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        userDetailsOny()
-    }, [user._id]);
-    console.log(currentUser);
+  //Fetching userDetail
+  const [currentUser, setCurrentUser] = useState({});
+  useEffect(() => {
+    const userDetailsOny = async () => {
+      try {
+        const res = await axios.post("/userDetails/get", {
+          userID: user._id,
+        });
+        setCurrentUser(res.data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    userDetailsOny();
+  }, [user._id]);
+  console.log(currentUser);
 
+  return (
+    <div className="userInfoCon">
+      <div className="userNameProfessionActive">
+        <span className="uiName">{user.fullName}</span>
+        <span className="uiProfession">{currentUser.profession}</span>
+        <span className="uiActive">active 5 hour ago</span>
+      </div>
 
-
-
-    return (
-        <div className="userInfoCon">
-
-            <div className="userNameProfessionActive">
-                <span className='uiName'>{user.fullName}</span>
-                <span className='uiProfession'>Software Engineer</span>
-                <span className="uiActive">active 5 hour ago</span>
-            </div>
-
-
-            <div className="uiFollowFollowing">
-                <div className="uiFollower">
-                    <span className='uiFollowerTxt'>Follower</span>
-                    <span className='uiFollowerNum'>86</span>
-                </div>
-
-                <div className="uiFollower">
-                    <span className='uiFollowerTxt'>Following</span>
-                    <span className='uiFollowerNum'>184</span>
-                </div>
-            </div>
-
-
-
-
-            <div className="uiDetailInfo">
-                <div className="uiDetailInfoItem">
-                    <BusinessCenterIcon className='uiIcon' />
-                    {/* <span className='uiDetailInfoValue'>Software Engineer at Lendis</span> */}
-                    <span className='uiDetailInfoValue'>{currentUser?.currentJobPosition}</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <BusinessCenterIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>Co-founder at Ask Buddie</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <BusinessCenterIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>former WordPress Engineer at Eagle Eye </span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <SchoolIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>Went to Oxfors Academy</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <LocationOnIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>{currentUser.homeTown}</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <NetworkWifi2BarIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>Followed by 995 people</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <BusinessCenterIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>Lokendra Chaulagain</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <GitHubIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>ashiishme</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <TwitterIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>ashiishme</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <LinkedInIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>ashiishme</span>
-                </div>
-
-
-                <div className="uiDetailInfoItem">
-                    <InstagramIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>ashiishme</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <InsertLinkIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>www.ashishme.com.mp</span>
-                </div>
-
-                <div className="uiDetailInfoItem">
-                    <MoreHorizIcon className='uiIcon' />
-
-                    <span className='uiDetailInfoValue'>See more about Ashish</span>
-                </div>
-            </div>
-
-            <div className="InterestHobbyTxtAndEditButCon">
-                <span className='interestHobby'>Interest and Hobby</span>
-                <button onClick={() => setShowUserInfoEditCon(!showUserInfoEditCon)} className="userInfoEditBut">Edit</button>
-            </div>
-
-            {showUserInfoEditCon && <UserInfoEdit />}
-
-
-            <div className="hobbyListCon">
-                <Hobby />
-                <Hobby />
-
-
-
-            </div>
-
-
-
-
-
+      <div className="uiFollowFollowing">
+        <div className="uiFollower">
+          <span className="uiFollowerTxt">Followers</span>
+          <span className="uiFollowerNum">{currentUser.followers}</span>
         </div>
-    )
+
+        <div className="uiFollower">
+          <span className="uiFollowerTxt">Following</span>
+          <span className="uiFollowerNum">{currentUser.following}</span>
+        </div>
+      </div>
+
+      <div className="uiDetailInfo">
+        <div className="uiDetailInfoItem">
+          <BusinessCenterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            {currentUser?.currentJobPosition}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <BusinessCenterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Founder at {currentUser.founderCoFounderAt}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <BusinessCenterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Co-founder at {currentUser.founderCoFounderAt}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <BusinessCenterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Former {currentUser.pastJobPosition} at {currentUser.pastJobAt}{" "}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <BusinessCenterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Former {currentUser.pastJobPosition} at {currentUser.pastJobAt}{" "}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <BusinessCenterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Former {currentUser.pastJobPosition} at {currentUser.pastJobAt}{" "}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <SchoolIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Currently Studying at {currentUser.currentlyStudying}{" "}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <SchoolIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Graduated from {currentUser.currentlyStudying}{" "}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <SchoolIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Completed HighSchooling from {currentUser.highSchool}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <SchoolIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Completed Schooling from {currentUser.school}
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <LocationOnIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">{currentUser.homeTown}</span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <NetworkWifi2BarIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            Followed by {currentUser.followerCount} people
+          </span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <GitHubIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">ashiishme</span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <TwitterIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">ashiishme</span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <LinkedInIcon className="uiIcon" />
+
+          <span className="uiDetailInfoValue">ashiishme</span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <InstagramIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">ashiishme</span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <InsertLinkIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">www.ashishme.com.mp</span>
+        </div>
+
+        <div className="uiDetailInfoItem">
+          <MoreHorizIcon className="uiIcon" />
+          <span className="uiDetailInfoValue">
+            See more about {user.fullName.split(" ")[0]}
+          </span>
+        </div>
+      </div>
+
+      <div className="InterestHobbyTxtAndEditButCon">
+        <span className="interestHobby">Interest and Hobby</span>
+        <button
+          onClick={() => setShowUserInfoEditCon(!showUserInfoEditCon)}
+          className="userInfoEditBut"
+        >
+          Edit
+        </button>
+      </div>
+
+      {showUserInfoEditCon && <UserInfoEdit />}
+
+      <div className="hobbyListCon">
+        <Hobby />
+        <Hobby />
+      </div>
+    </div>
+  );
 }
 
-export default UserInfo
+export default UserInfo;
--- client/src/pages/userProfile/UserProfile.js
+++ client/src/pages/userProfile/UserProfile.js
@@ -1,112 +1,114 @@
-import React, { useContext, useEffect, useState } from 'react'
-import "./userProfile.scss"
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import OndemandVideoOutlinedIcon from '@mui/icons-material/OndemandVideoOutlined';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import Badge from '@mui/material/Badge';
-import UserInfo from '../../components/userInfo/UserInfo';
-import FeedPost from '../../components/feedPost/FeedPost';
-import FeedPostCreate from '../../components/feedPostCreate/FeedPostCreate';
-import { Link } from 'react-router-dom'
-import Navbar from '../../components/navbar/Navbar'
-import LeftBar from '../../components/leftBar/LeftBar';
-import UserInfoEdit from '../../components/userInfoEdit/UserInfoEdit';
-// import axios from 'axios'
-// import { AuthContext } from '../../context/authContext/AuthContext';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import "./userProfile.scss";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import OndemandVideoOutlinedIcon from "@mui/icons-material/OndemandVideoOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import Badge from "@mui/material/Badge";
+import UserInfo from "../../components/userInfo/UserInfo";
+import FeedPost from "../../components/feedPost/FeedPost";
+import FeedPostCreate from "../../components/feedPostCreate/FeedPostCreate";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
+import LeftBar from "../../components/leftBar/LeftBar";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import axios from "axios";
 
 function UserProfile() {
+  //To open the FeedPostCreateContainer
+  const [showFeedPostCreateCon, setShowFeedPostCreateCon] = useState(false);
 
-    //To open the FeedPostCreateContainer
-    const [showFeedPostCreateCon, setShowFeedPostCreateCon] = useState(false)
+  const { user } = useContext(AuthContext);
 
-    //calling currentUser from AuthContext
-    // const { user } = useContext(AuthContext)
-    // console.log(user);
+  //Fetching userDetail
+  const [currentUser, setCurrentUser] = useState({});
+  useEffect(() => {
+    const userDetailsOny = async () => {
+      try {
+        const res = await axios.post("/userDetails/get", {
+          userID: user._id,
+        });
+        setCurrentUser(res.data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    userDetailsOny();
+  }, [user._id]);
+  console.log(currentUser);
 
-   
-
-    return (
-        <div className="userProfile">
-            <Navbar />
-            <div className="upWrapper">
-                <div className="upCoverPicCon">
-                    <img src="/assets/cover.jpeg" alt="" className="upCoverPic" />
-                    <img src="/assets/profile.jpeg" alt="" className="upProfilePic" />
-                </div>
-
-                <div className="upBeforeSplit">
-                    <div className="upLeftCon">
-                        <LeftBar />
-
-
-                    </div>
-                    <div className="upCenterCon">
-
-
-                        <div className="upRowIconCon">
-                            <div className="upRowIconItem">
-                                <Link to="/" className='link'>
-                                    <HomeOutlinedIcon color="action" />
-                                </Link>
-                            </div>
-
-                            <div className="upRowIconItem">
-                                <Badge badgeContent={4} color="primary">
-                                    <ChatOutlinedIcon color="action" />
-                                </Badge>
-                            </div>
-
-                            <div className="upRowIconItem">
-                                <Badge badgeContent={4} color="primary">
-                                    <NotificationsNoneOutlinedIcon color="action" />
-                                </Badge>
-                            </div>
-
-                            <div className="upRowIconItem">
-                                <Badge badgeContent={4} color="primary">
-                                    <PeopleAltOutlinedIcon color="action" />
-                                </Badge>
-                            </div>
-
-                            <div className="upRowIconItem">
-                                <Badge badgeContent={4} color="primary">
-                                    <OndemandVideoOutlinedIcon color="action" />
-                                </Badge>
-                            </div>
-                        </div>
-
-
-
-
-                        <div className="upSmallWhatsOnYourMindCon">
-                            <img src="assets/profile.jpeg" alt="" className="upTinyProfilePic" />
-                            <input onClick={() => setShowFeedPostCreateCon(!showFeedPostCreateCon)} type="text" className="upOnYourMindInput" placeholder="What's on your mind Lokendra ?  Post photos / videos / text from here" />
-                        </div>
-                        {/* On Click ma yo container show hunxa */}
-                        {showFeedPostCreateCon && <FeedPostCreate />}
-
-
-
-
-                        <FeedPost />
-
-                    </div>
-                    <div className="upRightCon">
-                        <UserInfo />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="userProfile">
+      <Navbar />
+      <div className="upWrapper">
+        <div className="upCoverPicCon">
+          <img src={currentUser.coverPic} alt="" className="upCoverPic" />
+          <img src={currentUser.profilePic} alt="" className="upProfilePic" />
         </div>
-    )
+
+        <div className="upBeforeSplit">
+          <div className="upLeftCon">
+            <LeftBar />
+          </div>
+          <div className="upCenterCon">
+            <div className="upRowIconCon">
+              <div className="upRowIconItem">
+                <Link to="/" className="link">
+                  <HomeOutlinedIcon color="action" />
+                </Link>
+              </div>
+
+              <div className="upRowIconItem">
+                <Badge badgeContent={4} color="primary">
+                  <ChatOutlinedIcon color="action" />
+                </Badge>
+              </div>
+
+              <div className="upRowIconItem">
+                <Badge badgeContent={4} color="primary">
+                  <NotificationsNoneOutlinedIcon color="action" />
+                </Badge>
+              </div>
+
+              <div className="upRowIconItem">
+                <Badge badgeContent={4} color="primary">
+                  <PeopleAltOutlinedIcon color="action" />
+                </Badge>
+              </div>
+
+              <div className="upRowIconItem">
+                <Badge badgeContent={4} color="primary">
+                  <OndemandVideoOutlinedIcon color="action" />
+                </Badge>
+              </div>
+            </div>
+
+            <div className="upSmallWhatsOnYourMindCon">
+              <img
+                src="assets/profile.jpeg"
+                alt=""
+                className="upTinyProfilePic"
+              />
+              <input
+                onClick={() => setShowFeedPostCreateCon(!showFeedPostCreateCon)}
+                type="text"
+                className="upOnYourMindInput"
+                placeholder="What's on your mind Lokendra ?  Post photos / videos / text from here"
+              />
+            </div>
+            {/* On Click ma yo container show hunxa */}
+            {showFeedPostCreateCon && <FeedPostCreate />}
+
+            <FeedPost />
+          </div>
+          <div className="upRightCon">
+            <UserInfo />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-
-
-export default UserProfile
+export default UserProfile;
