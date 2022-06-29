43a1975fdc7b13673123f68736616fb85f31c99f
lokendra <lokendrachaulagain803@gmail.com>
2022-06-29 09:52:09 +0545

    deployed

--- api/utils/error.js
+++
@@ -1,9 +0,0 @@
-//Error handling function for the API requests
-const createError = (status, message) => {
-  const error = new Error();
-  error.status = status;
-  error.message = message;
-  return error;
-};
-
-module.exports = { createError };
